ec1c014d322f7e7f153f491a282afd1ea930175f
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2024-01-28 19:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const form=document.querySelector("form");
+form.addEventListener("submit",onSubmitForm);
+function onSubmitForm(event){
+	const firstName=document.getElementById("firstName");
+	const lastName=document.getElementById("lastName");
+	const phoneNumber=document.getElementById("phoneNumber");
+	const email=document.getElementById("email");
+
+	const result="FirstName: "+firstName+"\nLastName: "+lastName+"\nPhone Number: "+phoneNumber+"\nEmail ID: "+email;
+	alert(result);
+	
+}
